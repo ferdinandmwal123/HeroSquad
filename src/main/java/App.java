fda4feefdc567a73968aaca0e31fab25c5e3106b
Your Name <ferdinandmwal@gmail.com>
2019-09-16 15:01:43 +0300

    Added pom.xml

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,6 +17,7 @@ import static spark.Spark.*;
 
 public class App{
     public static void main(String[] args) {
+
         staticFileLocation("/public");
         String connectionString = "jdbc:h2:~/herosquad.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
         Sql2o sql2o = new Sql2o(connectionString, "", "");
