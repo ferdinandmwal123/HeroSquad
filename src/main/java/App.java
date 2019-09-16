4b6fdeb15407f2cd0916bbb2055e1890bf7592d9
Your Name <ferdinandmwal@gmail.com>
2019-09-16 15:03:34 +0300

    Styled templates

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,7 +18,7 @@ import static spark.Spark.*;
 public class App{
     public static void main(String[] args) {
 static int getHerokuAssignedPort(){
-
+ProcessBuilder processBuilder = new ProcessBuilder();
         }
         staticFileLocation("/public");
         String connectionString = "jdbc:h2:~/herosquad.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
